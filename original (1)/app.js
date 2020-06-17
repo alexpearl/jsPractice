700e2302d5c178d724fa4eaf42bfaa8fda0fa21a
alexpearl <alexpearl14@gmail.com>
2020-06-17 16:44:59 -0400

    refactoring yesterdays work

--- original (1)/app.js
+++ original (1)/app.js
@@ -85,28 +85,37 @@ const warriorsGames = [
   },
 ];
 
-const ulParent = document.createElement("ul"); //where to append all lis too
-for (let game of warriorsGames) {
-  const { homeTeam, awayTeam } = game; //destructuring
-  const gameLi = document.createElement("li");
-
-  const { team: hTeam, points: hPoints } = homeTeam; //destructing to avoid repetitive code below
-  const { team: aTeam, points: aPoints } = homeTeam;
-  // const teamNames = `${awayTeam.team} @ ${homeTeam.team}`;
-  // const scoreLine = `${awayTeam.points} - ${homeTeam.points}`;
-  const teamNames = `${aTeam} @ ${hTeam}`;
-
-  let scoreline;
-  if (aPoints > hPoints) {
-    scoreLine = `<b>${aPoints}</b> - ${hPoints}`;
-  } else {
-    scoreLine = `${aPoints} - <b>${hPoints}</b>`;
+const makeChart = (games, targetTeam) => {
+  const ulParent = document.createElement("ul");
+  for (let game of games) {
+    const gameLi = document.createElement("li");
+    gameLi.innerHTML = getScoreLine(game);
+    gameLi.classList.add(isWinner(game, targetTeam) ? "win" : "loss");
+    ulParent.appendChild(gameLi);
   }
+  return ulParent;
+};
+const isWinner = ({ homeTeam, awayTeam }, targetTeam) => {
+  const target = homeTeam.team === targetTeam ? homeTeam : awayTeam;
+  return target.isWinner;
+};
+const getScoreLine = ({ homeTeam, awayTeam }) => {
+  const { team: hTeam, points: hPoints } = homeTeam;
+  const { team: aTeam, points: aPoints } = awayTeam;
+  const teamNames = `${aTeam} @ ${hTeam}`;
+  let scoreLine;
+  if (aPoints > hPoints) {
+    scoreLine = `<b>${aPoints}</b>-${hPoints}`;
+  } else {
+    scoreLine = `${aPoints}-<b>${hPoints}</b>`;
+  }
+  return `${teamNames} ${scoreLine}`;
+};
 
-  const warriors = hTeam === "Golden State" ? homeTeam : awayTeam;
-  gameLi.classList.add(warriors.isWinner ? "win" : "loss"); //adding win or loss class if warriors are the winner
+const gsSection = document.querySelector("#gs");
+const hrSection = document.querySelector("#hr");
 
-  gameLi.innerHTML = `${teamNames} ${scoreLine}`;
-  ulParent.appendChild(gameLi);
-}
-document.body.prepend(ulParent);
+const gsChart = makeChart(warriorsGames, "Golden State");
+const hrChart = makeChart(warriorsGames, "Houston");
+gsSection.appendChild(gsChart);
+hrSection.appendChild(hrChart);
